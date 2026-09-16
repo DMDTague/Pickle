b6e6108fdbcfdc9b0c58cad6b000db920e7ed966
Dylan Tague <161772172+DMDTague@users.noreply.github.com>
2026-09-16 07:35:54 -0400

    Fix Emscripten time header collision

--- time_manager.cpp
+++ time_manager.cpp
@@ -1,5 +1,5 @@
-#include "time_manager.h"
 #include <chrono>
+#include "time_manager.h"
 
 TimeManager tm;
 
@@ -21,19 +21,18 @@ void set_time_limits(long long time_left, long long inc, long long movetime, int
         tm.max_time = movetime;
         tm.stop_time = tm.start_time + movetime;
     } else if (time_left > 0) {
-        // Allocate ~30 moves remaining + exact increment for safety
+        // Allocate roughly one move from a 30-move horizon plus part of increment.
         tm.optimum_time = (time_left / 30) + (inc / 2);
         tm.max_time = (time_left / 10) + inc;
 
         if (tm.optimum_time > time_left - 50) tm.optimum_time = time_left - 50;
         if (tm.max_time > time_left - 50) tm.max_time = time_left - 50;
-        
+
         if (tm.optimum_time < 0) tm.optimum_time = 0;
         if (tm.max_time < 0) tm.max_time = 0;
-        
-        tm.stop_time = tm.start_time + tm.max_time; // Hard stop check_time triggers on max
+
+        tm.stop_time = tm.start_time + tm.max_time;
     } else {
-        // Infinite time search (will be stopped externally or heavily bounded by depth)
         tm.optimum_time = -1;
         tm.max_time = -1;
         tm.stop_time = -1;
@@ -45,12 +44,9 @@ void check_time() {
         tm.time_is_up = true;
         return;
     }
-    
-    // Only check if we actually have a stop limit
-    if (tm.stop_time != -1) {
-        if (get_time_ms() >= tm.stop_time) {
-            tm.time_is_up = true;
-            tm.stopped = true;
-        }
+
+    if (tm.stop_time != -1 && get_time_ms() >= tm.stop_time) {
+        tm.time_is_up = true;
+        tm.stopped = true;
     }
 }
