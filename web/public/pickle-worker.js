d8d6672a7866917ee7db4e787c73accc346ae8b9
Dylan Tague <161772172+DMDTague@users.noreply.github.com>
2026-09-16 07:29:04 -0400

    Run Pickle WASM off the UI thread

---
+++ web/public/pickle-worker.js
@@ -0,0 +1,61 @@
+let modulePromise = null;
+
+async function getEngine() {
+  if (!modulePromise) {
+    importScripts('/engine/pickle.js');
+    modulePromise = self.createPickleModule({
+      locateFile: (path) => `/engine/${path}`,
+      noInitialRun: true,
+    }).then((engine) => {
+      engine.ccall('pickle_init', null, [], []);
+      return engine;
+    });
+  }
+  return modulePromise;
+}
+
+self.onmessage = async (event) => {
+  const message = event.data || {};
+
+  if (message.type === 'init') {
+    try {
+      await getEngine();
+      self.postMessage({ type: 'ready' });
+    } catch (error) {
+      self.postMessage({ type: 'error', message: String(error) });
+    }
+    return;
+  }
+
+  if (message.type !== 'search') return;
+
+  try {
+    const engine = await getEngine();
+    const move = engine.ccall(
+      'pickle_best_move',
+      'string',
+      ['string', 'number', 'number'],
+      [message.fen, message.depth, message.movetime],
+    );
+
+    const score = engine.ccall('pickle_last_score', 'number', [], []);
+    const depth = engine.ccall('pickle_last_depth', 'number', [], []);
+    const nodes = engine.ccall('pickle_last_nodes', 'number', [], []);
+
+    self.postMessage({
+      type: 'result',
+      requestId: message.requestId,
+      fen: message.fen,
+      move,
+      score,
+      depth,
+      nodes,
+    });
+  } catch (error) {
+    self.postMessage({
+      type: 'error',
+      requestId: message.requestId,
+      message: String(error),
+    });
+  }
+};
